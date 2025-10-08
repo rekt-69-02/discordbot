23694eb442449c207f84d8b97096365806c7e476
sfghsslkgfshg <bluezenithlol@gmail.com>
2025-10-08 14:15:46 +0800

    ay

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-import discord, json, os
+import discord, json, os, asyncio
 from discord.ext import commands
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
+async def main():
+    async with bot:
+        for cogs in os.listdir('./cogs'):
+            if cogs.endswith(".py"):
+                await bot.load_extension(f'cogs.{cogs[:-3]}')
+        with open('token.json', 'r') as jfile:
+            a = json.load(jfile)
+            bot.run(a['token'])    
+
 @bot.event
 async def on_ready():
-    for cogs in os.listdir('./cogs'):
-        if cogs.endswith(".py"):
-            await bot.load_extension(f'cogs.{cogs[:-3]}')
     # await bot.tree.sync()
     print("I'm ready to cum")
+
+@bot.command()
+async def sync(ctx: commands.Context):
+    s = await bot.tree.sync()
+    print(f"synced {s} commands")
 
 @bot.command()
 async def reload(ctx: commands.Context):
@@ -19,6 +31,5 @@ async def reload(ctx: commands.Context):
     print('reloaded')
     await ctx.send('reloaded')
 
-with open('token.json', 'r') as jfile:
-    a = json.load(jfile)
-    bot.run(a['token'])
+if __name__ == "__main__":
+    asyncio.run(main())
